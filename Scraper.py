b5411c8c6cf985cc3eb6503b10e982d1c4793c5e
aryan_r <raiaryan2420@gmail.com>
2020-12-25 19:47:21 +0530

    Added some documentation

--- Scraper.py
+++ Scraper.py
@@ -22,25 +22,31 @@ def NodeJS():
 
     soup = BeautifulSoup(page.content, 'html.parser')
 
-    results = soup.find(id='main')
-    # print(results.prettify())
-
+    results = soup.find(id='main') # Finds the content wrapped inside the tag with id main
+    
+    # Finds the p tag with the class color-lightgray from the results obtained from above
     required_results = results.find_all('p', class_='color-lightgray')
 
     # print(required_results)
 
     for required in required_results:
-        version_name = required.find('strong')
+        
+        # Finds the strong tag
+        version_name = required.find('strong') 
         if None in version_name:
             continue
-        print("Latest version of Node is:", version_name.text.strip())
+        
+        # Getting the text inside the strong tag
+        print("Latest version of Node is:", version_name.text.strip()) 
 
+        # Runs the node -v command and gets the printed version number 
         initial_version = str(subprocess.check_output(['node', '-v']).strip()).split("'")
 
         data = {"Date/Time": str(datetime.datetime.now()),
                 "Version-fetched": version_name.text.strip(),
                 "Initial-version": initial_version[1]}
 
+        # Writing the dictionary data to the log.txt file in append mode
         with open("log.txt", "a") as write_file:
             write_file.write("\n")
             write_file.write(str(data))
