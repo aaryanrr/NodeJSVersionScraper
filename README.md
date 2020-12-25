2465c24096c21af6ef90d5493e947f83e4be316e
aryan_r <raiaryan2420@gmail.com>
2020-12-25 19:36:56 +0530

    Added files

---
+++ README.md
@@ -0,0 +1,18 @@
+# WebScraper for getting the latest Version Number of NodeJS
+
+---
+
+## Description
+
+> It goes to the NodeJS Download Page, [here](https://nodejs.org/en/download/) and scrapes the version number and stores the details like Date/Time of when the program was ran and the fetched version from the website and the initial Version that is installed on the system to the *log.txt* file
+
+
+## Environment Setup
+
+
+
+> *Note: It is recommended that you create a Virtual Environment to run this program as it would avoid any conflicts or errors in Package Installation*
+
+1. `git clone https://github.com/aaryanrr/NodeJSVersionScraper.git`
+2. `pip3 install -r requirements.txt`
+3. `py Scraper.py`
---
+++ Scraper.py
@@ -0,0 +1,51 @@
+import requests
+from bs4 import BeautifulSoup
+import datetime
+import subprocess
+import os
+
+
+def CheckLog():
+    ans = os.path.exists("log.txt")
+    if ans is True:
+        pass
+    else:
+        file_create = open("log.txt", "w+")
+        file_create.close()
+
+
+def NodeJS():
+    URL = "https://nodejs.org/en/download/"
+
+    page = requests.get(URL)
+    # print("Respone code is:", page)
+
+    soup = BeautifulSoup(page.content, 'html.parser')
+
+    results = soup.find(id='main')
+    # print(results.prettify())
+
+    required_results = results.find_all('p', class_='color-lightgray')
+
+    # print(required_results)
+
+    for required in required_results:
+        version_name = required.find('strong')
+        if None in version_name:
+            continue
+        print("Latest version of Node is:", version_name.text.strip())
+
+        initial_version = str(subprocess.check_output(['node', '-v']).strip()).split("'")
+
+        data = {"Date/Time": str(datetime.datetime.now()),
+                "Version-fetched": version_name.text.strip(),
+                "Initial-version": initial_version[1]}
+
+        with open("log.txt", "a") as write_file:
+            write_file.write("\n")
+            write_file.write(str(data))
+        break
+
+
+CheckLog()
+NodeJS()
